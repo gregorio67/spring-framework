83738907209b352bb209a13a9fc741baefd81b08
gregorio67 <kkimdoy@gmail.com>
2018-01-30 16:15:48 +0900

    Create ClientRestService.java

---
+++ src/main/java/service/rest/ClientRestService.java
@@ -0,0 +1,7 @@
+
+import java.util.Map;
+
+public interface ClientRestService {
+
+	public abstract <T> T  callRestService(String systemId, String infId, String serviceName, Map<String, Object> params, Map<String, Object> transLog) throws Exception;
+}
